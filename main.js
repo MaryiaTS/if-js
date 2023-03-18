b8635e0160ed5ed193f226abc3be9df763a3cde4
Мария <marusya2010@tut.by>
2023-03-19 00:46:59 +0300

    Lesson-7 wrote the function that compares two objects

--- main.js
+++ main.js
@@ -1,77 +1,57 @@
-//Преобразование формата даты
-function transformDate(date) {
-  const re = /(?<year>\d+)+-+(?<month>\d+)+-+(?<day>\d+)/gm;
-  const newDate = date
-    .replace(re, "$<day> $<month> $<year>")
-    .replace(/\s/g, ".");
-  return (newDate);
-}
+const obj1 = {
+    a: 'a',
+    b: {
+        a: 'a',
+        b: 'b',
+        c: {
+            a: 1,
+        },
+    },
+};
+const obj2 = {
+    b: {
+        c: {
+            a: 1,
+        },
+        b: 'b',
+        a: 'a',
+    },
+    a: 'a',
+};
+const obj3 = {
+    a: {
+        c: {
+            a: 'a',
+        },
+        b: 'b',
+        a: 'a',
+    },
+    b: 'b',
+};
 
-const date1 = "2020-11-26";
-console.log(transformDate(date1)); // 26.11.2020
-
-const date2 = "2020-1-5";
-console.log(transformDate(date2)); // 5.1.2020
-
-const date3 = "22-01-5";
-console.log(transformDate(date3)); // 5.01.22
-
-//Поиск объектов размещения:
-function searchData(word) {
-  const data = [
-    {
-      country: "Russia",
-      city: "Saint Petersburg",
-      hotel: "Hotel Leopold",
-    },
-    {
-      country: "Spain",
-      city: "Santa Cruz de Tenerife",
-      hotel: "Apartment Sunshine",
-    },
-    {
-      country: "Slowakia",
-      city: "Vysokie Tatry",
-      hotel: "Villa Kunerad",
-    },
-    {
-      country: "Germany",
-      city: "Berlin",
-      hotel: "Hostel Friendship",
-    },
-    {
-      country: "Indonesia",
-      city: "Bali",
-      hotel: "Ubud Bali Resort&SPA",
-    },
-    {
-      country: "Netherlands",
-      city: "Rotterdam",
-      hotel: "King Kong Hostel",
-    },
-    {
-      country: "Marocco",
-      city: "Ourika",
-      hotel: "Rokoko Hotel",
-    },
-    {
-      country: "Germany",
-      city: "Berlin",
-      hotel: "Hotel Rehberge Berlin Mitte",
-    },
-  ];
-  for (let i = 0; i < data.length; i++) {
-    if (
-      data[i].country === word ||
-      data[i].city === word ||
-      data[i].hotel === word
-    ) {
-      return [data[i].country, data[i].city, data[i].hotel].join(", ");
+function deepEqual(a, b) {
+    if (a === b) {
+        return true;
     }
-  }
+
+    if (a == null || typeof(a) != "object" ||
+        b == null || typeof(b) != "object")
+    {
+        return false;
+    }
+
+    let propertiesInA = 0, propertiesInB = 0;
+    for (property in a) {
+        propertiesInA += 1;
+    }
+    for (property in b) {
+        propertiesInB += 1;
+        if (!(property in a) || !deepEqual(a[property], b[property])) {
+            return false;
+        }
+    }
+    return propertiesInA === propertiesInB;
 }
 
-console.log(searchData("London"));
-console.log(searchData("Spain"));
-console.log(searchData("King Kong Hostel"));
-console.log(searchData("Berlin"));
+console.log(deepEqual(obj1, obj2)); // true
+console.log(deepEqual(obj1, obj3)); // false
